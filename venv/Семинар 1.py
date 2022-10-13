ab578cb6c50aa7aace06004d9dab04963975c2e8
Dan Kodin <dan_kodin03@mail.ru>
2022-10-13 18:18:27 +0300

    Реализована задача про четверти.

--- venv/Семинар 1.py
+++ venv/Семинар 1.py
@@ -50,3 +50,19 @@ elif x < 0 and y < 0:
     print("Четверть: " + 3)
 elif x > 0 and y < 0:
     print("Четверть: " + 4)
+
+# Задача про четверти: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных
+# координат точек в этой четверти (x и y).
+
+print()
+print("Задача про четверти: ")
+print("Введите номер четверти от 1 до 4: ")
+num = int(input())
+if num == 1:
+    print("x = (0;infinity), y = (0;infinity)")
+elif num == 2:
+    print("x = (0;-infinity), y = (0;infinity)")
+elif num == 3:
+    print("x = (0;-infinity), y = (0;-infinity)")
+elif num == 4:
+    print("x = (0;infinity), y = (0;-infinity)")
