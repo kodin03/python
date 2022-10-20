d62dab9871a5ba4735be1a865bc474610324ce62
Dan Kodin <dan_kodin03@mail.ru>
2022-10-20 19:59:22 +0300

    Задача про произведение пар чисел.

--- venv/Scripts/Семинар 3.py
+++ venv/Scripts/Семинар 3.py
@@ -3,7 +3,7 @@
 
 print("Задача про нечетные числа: ")
 inp = int(input("Введите количество чисел в списке: "))
-numbers = [inp]
+numbers = []
 
 def fun(inp):
     summa = 0
@@ -17,3 +17,36 @@ def fun(inp):
     print(f"Сумма нечетных элементов: {int(summa)}")
 
 fun(inp)
+
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент,
+# второй и предпоследний и т.д.
+
+print("Задача про пары чисел: ")
+inp = int(input("Введите количество чисел в списке: "))
+
+def inputData(inp):
+    numbers = []
+    answer = []
+    n = 1
+    print("Введите числа через пробел: ")
+    str1 = str(input())
+    numbers = [int(x) for x in str1.split(" ")]
+
+    for i in range(len(numbers)):
+        if len(numbers) % 2 == 1:
+            if i == len(numbers) - n:
+                answer.append(numbers[i] * numbers[len(numbers) - n])
+                break
+            elif i < n:
+                answer.append(numbers[i] * numbers[len(numbers) - n])
+                n += 1
+        elif len(numbers) % 2 == 0:
+            if i == len(numbers) - n - 1:    # 0 3   1 2  2 1
+                answer.append(numbers[i] * numbers[len(numbers) - n])
+                break
+            elif i < n:
+                answer.append(numbers[i] * numbers[len(numbers) - n])
+                n += 1
+    print(f"Произведение парных чисел: {answer}")
+
+inputData(inp)
