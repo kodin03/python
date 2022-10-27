3be9222ffbd5df87724958e5ff1725d76af2250a
Dan Kodin <dan_kodin03@mail.ru>
2022-10-27 12:50:08 +0300

    Добавлен многочлен для следующей задачи.

--- venv/Семинар 4/Задача 3.py
+++ venv/Семинар 4/Задача 3.py
@@ -6,7 +6,6 @@ import itertools
 
 print("Задача про многочлен: ")
 k = randint(2, 4)
-print(k)
 
 def get_koef(k):
     koef = [randint(0, 10) for i in range (k + 1)]
@@ -15,11 +14,8 @@ def get_koef(k):
     return koef
 
 def get_polynomial(k, koef):
-    print(koef)
     var = ['*x^']*(k-1) + ['*x']
     polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(koef, var, range(k, 1, -1), fillvalue = '') if a != 0]
-    print(polynomial)
-
     for x in polynomial:
         x.append(' + ')
     polynomial = list(itertools.chain(*polynomial))
@@ -33,5 +29,15 @@ print(polynom1)
 
 with open('Задача 3.txt', 'w') as data:
     data.write(polynom1)
+
+# Для следующей задачи
+k = randint(2, 5)
+
+koef = get_koef(k)
+polynom2 = get_polynomial(k, koef)
+print(polynom2)
+
+with open('Задача 3.2.txt', 'w') as data:
+    data.write(polynom2)
 
 
