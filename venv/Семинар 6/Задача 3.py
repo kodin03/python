a9d2f3eeef982613e6c3f3aae26d622e4f27e4ae
Dan Kodin <dan_kodin03@mail.ru>
2022-11-15 20:03:16 +0300

    Использованы List Comprehessions и zip().

---
+++ venv/Семинар 6/Задача 3.py
@@ -0,0 +1,77 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+import re
+import itertools
+
+
+file1 = 'Задача 3_1.txt'
+file2 = 'Задача 3_вывод.txt'
+file_sum = 'Сумма многочленов.txt'
+
+# Получение данных из файла
+
+def read_file(file):
+    with open(str(file), 'r') as data:
+        f = data.read()
+    return f
+
+# Получение списка кортежей каждого (<коэффициент>, <степень>)
+
+def convert_file(f):
+    f = f.replace('= 0', '')
+    f = re.sub("[*|^| ]", " ", f).split('+')
+    f = [char.split(' ') for char in f]
+    f = [[x for x in list if x] for list in f]
+    for i in f:
+        if i[0] == 'x': i.insert(0, 1)
+        if i[-1] == 'x': i.append(1)
+        if len(i) == 1: i.append(0)
+    f = [tuple(int(x) for x in j if x != 'x') for j in f]
+    return f
+
+# Получение списка кортежей суммы (<коэф1 + коэф2>, <степень>)
+
+def fold_fs(f1, f2):
+    x = [0] * (max(f1[0][1], f2[0][1] + 1))
+    for i in f1 + f2:
+        x[i[1]] += i[0]
+    res = [(x[i], i) for i in range(len(x)) if x[i] != 0]
+    res.sort(key = lambda r: r[1], reverse = True)
+    return res
+
+# Составление итогового многочлена
+
+def get_sum_f(f):
+    var = ['*x^'] * len(f)
+    koefs = [x[0] for x in f]
+    degrees = [x[1] for x in f]
+    new_f = [[str(a), str(b), str(c)] for a, b, c in (zip(koefs, var, degrees))]
+    for x in new_f:
+        if x[0] == '0': del (x[0])
+        if x[-1] == '0': del (x[-1], x[-1])
+        if len(x) > 1 and x[0] == '1' and x[1] == '*x^': x[1:], x[1:][1:]
+        if len(x) > 1 and x[-1] == '1':
+            del x[-1]
+            x[-1] = '*x'
+        x.append(' + ')
+    new_f = list(itertools.chain(*new_f))
+    new_f[-1] = ' = 0'
+    return "".join(map(str, new_f))
+
+def write_to_file(file, f):
+    with open(file, 'w') as data:
+        data.write(f)
+
+
+f1 = read_file(file1)
+f2 = read_file(file2)
+f_1 = convert_file(f1)
+f_2 = convert_file(f2)
+sum = get_sum_f(fold_fs(f_1, f_2))
+
+write_to_file(file_sum, sum)
+
+print(f"Первое исходное уравнение: {f1}")
+print(f"Второе исходное уравнение: {f2}")
+print(f"Сумма: {sum}")
+
